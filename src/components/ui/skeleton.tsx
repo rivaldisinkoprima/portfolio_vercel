ababce7090a934fbcafb1cabeaf580dfe05dff8d
rivaldisinkoprima <rivaldi.sinkoprima@gmail.com>
2026-02-24 13:49:27 +0700

    shimmer effect

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -10,7 +10,7 @@ export function Skeleton({ className }: SkeletonProps) {
   return (
     <div
       className={cn(
-        "animate-pulse bg-muted rounded-md",
+        "animate-shimmer rounded-md",
         className
       )}
     />
